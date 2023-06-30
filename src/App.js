772804ce666271d2e1b6a29d3261c706b2e18a33
wilver_alinan <will28_alinan@hotmail.com>
2023-06-29 21:20:48 -0600

    tarea4 CRUD

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Formulario from './components/Formulario'
+import Tarea4 from './components/Tarea4'
+
+
+const App = () => {
+  return (
+    <div>
+      {/* <Formulario></Formulario> */}
+      <Tarea4/>
+      
+
+    </div>
+  )
+}
+
+export default App
